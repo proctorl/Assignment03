8c6c47d76e694055bdafcb736ef81a956fa7fa5d
proctorl <54678938+proctorl@users.noreply.github.com>
2019-09-28 23:26:07 -0500

    Initial commit

---
+++ Assets/Scripts/GameName.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameName : MonoBehaviour
+{
+    public Text highScore;
+
+    public void Update()
+    {
+        highScore.text = PlayerName.ShowName + "'s Score: ";
+    }
+}
---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour {
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    public void ExitGame()
+    {
+
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+    public void HighScores()
+    {
+        SceneManager.LoadScene("HighScores");
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Play");
+    }
+
+}
